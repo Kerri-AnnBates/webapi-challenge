5271ec9c9607ec37ba26514e9de74e06449455ab
Kerri-Ann <kaybee1jam@yahoo.com>
2019-12-06 12:32:43 -0500

    set up GET and DELETE requests for projects

--- api/actions-route.js
+++ api/actions-route.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express();
 
 const actions = require('../data/helpers/actionModel');
+const projects = require('../data/helpers/projectModel');
 
 // GET actions.
 router.get('/:id', validateId, (req, res) => {
@@ -26,15 +27,42 @@ router.delete('/:id', validateId, (req, res) => {
         .catch(error => {
             res.status(500).json({ message: 'Unable to delete actions' });
         })
-})
+});
+
+// POST an action. When adding an action, make sure the project_id provided belongs to an existing project
+router.post('/', (req, res) => {
+    const newAction = req.body;
+    /* 
+        {
+            "project_id": 5,
+            "description": "Action description"
+        }
+    */
+
+    // get projects and see if the action's id matches one of the project's id.
+    projects.get(newAction.project_id)
+        .then(project => { 
+            if(!project) { 
+                res.status(404).json({ message: 'No such project Id exist. Unable to add action.' }); 
+            }
+        });
+
+    actions.insert(newAction)
+        .then(actionAdded => {
+            res.status(200).json(actionAdded);
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'Unable to add action' });
+        })
+});
 
 // Custom middlewar
 function validateId(req, res, next) {
     const id = req.params.id;
 
     actions.get(id)
-        .then(project => {
-            if (!project) {
+        .then(action => {
+            if (!action) {
                 res.status(404).json({ message: 'No such action exists' });
             } else {
                 next();
--- api/projects-route.js
+++ api/projects-route.js
@@ -3,8 +3,11 @@ const router = express();
 
 const projects = require('../data/helpers/projectModel');
 
-router.get('/', (req, res) => {
-    projects.get()
+// GET
+router.get('/:id', validateId, (req, res) => {
+    const id = req.params.id;
+
+    projects.get(id)
         .then(project => {
             res.status(200).json(project);
         })
@@ -12,5 +15,32 @@ router.get('/', (req, res) => {
             res.status(500).json({ message: 'Unable to retrieve projects' });
         })
 });
+
+// DELETE
+router.delete('/:id', validateId, (req, res) => {
+    const id = req.params.id;
+
+    projects.remove()
+        .then(removedItem => {
+            res.status(200).json(removedItem);
+        })
+        .catch(error => {
+            res.status(500).json({ message: 'Unable to delete project' });
+        })
+});
+
+// Custom middlewar
+function validateId(req, res, next) {
+    const id = req.params.id;
+
+    projects.get(id)
+        .then(project => {
+            if (!project) {
+                res.status(404).json({ message: 'No such project exists' });
+            } else {
+                next();
+            }
+        })
+}
 
 module.exports = router;
