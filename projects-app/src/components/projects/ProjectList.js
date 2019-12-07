25fca925c6c05df4963b651445f78faa9805098c
Kerri-Ann <kaybee1jam@yahoo.com>
2019-12-07 15:23:54 -0500

    added "add" button

--- projects-app/src/components/projects/ProjectList.js
+++ projects-app/src/components/projects/ProjectList.js
@@ -22,7 +22,10 @@ function ProjectList() {
 
     return (
         <div className="project-wrapper">
+            <div>
             <h2>Your Projects</h2>
+            <button className="btn btn-add">Add</button>
+            </div>
             {projects.map(project => (
                 <Project
                     key={project.id}
